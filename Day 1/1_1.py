a96b5f582ec63ec112d9f4b63e82e607f86ceeef
Sun Yudong <sunjerry019@gmail.com>
2018-12-09 15:29:11 +0800

    organize

---
+++ Day 1/1_1.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+input = open("1.in","r")
+inputContents = input.readlines()
+
+freq = 0
+
+for line in inputContents:
+    freq =  freq + int(line)
+
+print(freq)
---
+++ Day 1/1_2.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+input = open("1.in","r")
+inputContents = input.readlines()
+#inputContents = "+7, +7, -2, -7, -4".split(", ")
+
+freq = 0
+foundFreqs = [0]
+found = False
+
+loops = 0
+
+while not found:
+    loops = loops + 1
+    for line in inputContents:
+        freq = freq + int(line)
+        print("Loop {}\tCurrent Freq {} Hz          ".format(loops, freq), end='\r', flush=True)
+        if freq in foundFreqs:
+            found = True
+            print("Loop {}\tCurrent Freq {} Hz < encountered again >\n".format(loops, freq), flush=True)
+            break
+        else:
+            foundFreqs.append(freq)
+
+print(freq)
