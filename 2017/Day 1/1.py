724c161f72445cd021fe327a6e4c7df4ab7b84bd
Sun Yudong <sunjerry019@gmail.com>
2018-12-11 14:15:22 +0800

    pad 0

---
+++ 2017/Day 1/1.py
@@ -0,0 +1,3 @@
+#!/bin/python3
+
+inputContents = open("10.in","r").readlines()[0]
--- newday.sh
+++ newday.sh
@@ -2,6 +2,7 @@
 
 echo "Creating for Day $2 for year $1"
 
+$1 = `printf %02d $1`
 mkdir -p "$1/day_$2"
 cd "$1/$2"
 touch "$2_1.py" "$2_2.py" "$2.in" "$2.txt"
