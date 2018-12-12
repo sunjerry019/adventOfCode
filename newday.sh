614d90a08a9c1aab4d02b56efcb145311d6317eb
Sun Yudong <sunjerry019@gmail.com>
2018-12-13 07:55:05 +0800

    fixed spaces between assignment

--- newday.sh
+++ newday.sh
@@ -2,7 +2,7 @@
 
 echo "Creating for Day $2 for year $1"
 
-$1 = `printf %02d $1`
-mkdir -p "$1/day_$2"
-cd "$1/$2"
+DAY=`printf %02d $2`
+mkdir -p "$1/day_$DAY"
+cd "$1/day_$DAY"
 touch "$2_1.py" "$2_2.py" "$2.in" "$2.txt"
