87875536de5a3725339517c4605c2c686fd42586
Sun Yudong <sunjerry019@gmail.com>
2018-12-14 14:32:08 +0800

    Day 14

--- 2018/day_14/14_2.py
+++ 2018/day_14/14_2.py
@@ -18,17 +18,21 @@ class Problem():
             print(len(self.recipeScores), end="\r")
             score = self.recipeScores[self.elfRecipe[0]] + self.recipeScores[self.elfRecipe[1]]
             # print(self.recipeScores[self.elfRecipe[0]], self.recipeScores[self.elfRecipe[1]])
-            try:
-                newRecipes = [(score//(10**i))%10 for i in range(math.ceil(math.log(score, 10)+0.001)-1, -1, -1)]
-            except:
-                newRecipes = [0]
+            # try:
+            #     newRecipes = [(score//(10**i))%10 for i in range(math.ceil(math.log(score, 10)+0.001)-1, -1, -1)]
+            # except:
+            #     newRecipes = [0]
+            if score > 9:
+                newRecipes = [math.floor(score/10), score%10]
+            else:
+                newRecipes = [score]
 
             for digit in newRecipes:
                 self.recipeScores.append(digit)
                 if len(self.recipeScores) <= len(self.input):
                     self.lastInsertion = self.recipeScores
                 else:
-                    self.lastInsertion = self.recipeScores[-5:]
+                    self.lastInsertion = self.recipeScores[-len(self.input):]
                 if self.lastInsertion == self.input:
                     self.found = True
                     print(len(self.recipeScores) - len(self.input))
