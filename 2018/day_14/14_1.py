373c0481a6137446e975a335f59b50034c09b389
Sun Yudong <sunjerry019@gmail.com>
2018-12-14 14:18:26 +0800

    day 14, doesnt work for part b

---
+++ 2018/day_14/14_1.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+
+class Problem():
+    def __init__ (self):
+        self.afterRecipe = 74501 + 10
+        # self.afterRecipe = 19
+        self.recipeScores = [3, 7]
+        self.elfRecipe = [0, 1]
+
+        while len(self.recipeScores) < self.afterRecipe:
+            score = self.recipeScores[self.elfRecipe[0]] + self.recipeScores[self.elfRecipe[1]]
+            # print(self.recipeScores[self.elfRecipe[0]], self.recipeScores[self.elfRecipe[1]])
+            try:
+                newRecipes = [(score//(10**i))%10 for i in range(math.ceil(math.log(score, 10)+0.001)-1, -1, -1)]
+            except:
+                newRecipes = [0]
+            self.recipeScores += newRecipes
+
+            # Move Elves
+            l = len(self.recipeScores)
+            for i in range(2):
+                self.elfRecipe[i] += 1 + self.recipeScores[self.elfRecipe[i]]
+                self.elfRecipe[i] %= l
+
+        for i in range(self.afterRecipe - 10, self.afterRecipe):
+            print(self.recipeScores[i], end="")
+        print("")
+Problem()
---
+++ 2018/day_14/14_2.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+
+class Problem():
+    def __init__ (self):
+        self.rawinput = "074501"
+        # self.rawinput = "51589"
+        # self.rawinput = "59414"
+        self.input = [int(x) for x in self.rawinput]
+        # self.afterRecipe = 19
+        self.recipeScores = [3, 7]
+        self.elfRecipe = [0, 1]
+        self.lastInsertion = list()
+        self.found = False
+
+        while not self.found:
+            print(len(self.recipeScores), end="\r")
+            score = self.recipeScores[self.elfRecipe[0]] + self.recipeScores[self.elfRecipe[1]]
+            # print(self.recipeScores[self.elfRecipe[0]], self.recipeScores[self.elfRecipe[1]])
+            try:
+                newRecipes = [(score//(10**i))%10 for i in range(math.ceil(math.log(score, 10)+0.001)-1, -1, -1)]
+            except:
+                newRecipes = [0]
+
+            for digit in newRecipes:
+                self.recipeScores.append(digit)
+                if len(self.recipeScores) <= len(self.input):
+                    self.lastInsertion = self.recipeScores
+                else:
+                    self.lastInsertion = self.recipeScores[-5:]
+                if self.lastInsertion == self.input:
+                    self.found = True
+                    print(len(self.recipeScores) - len(self.input))
+                    break;
+
+            # Move Elves
+            l = len(self.recipeScores)
+            for i in range(2):
+                self.elfRecipe[i] += 1 + self.recipeScores[self.elfRecipe[i]]
+                self.elfRecipe[i] %= l
+
+Problem()
